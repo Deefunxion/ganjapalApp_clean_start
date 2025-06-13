817709235d2f77cb17037003490684c387ea2043
Dimitris Papadopoulos <deefunxion@gmail.com>
2025-06-13 19:17:11 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/GanjapalApp_clean_start/",
+  base: "/GanjapalApp/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
